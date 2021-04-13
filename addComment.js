82fc3c3fc51aa16e4e216a01cc1220b7e4a952e4
Anis Rb <47614271+bomarto5m1m0@users.noreply.github.com>
2021-04-12 23:08:04 -0700

    Add files via upload

---
+++ addComment.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react',
+import import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+
+class addComment extends Component {
+    constructor (props){
+        super(props);
+        const commentNumber = this.campsite.comment
+    }
+}
